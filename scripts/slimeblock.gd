3d4abbc03df659e28659dcb9251efa1e661e30b0
EpsilonTheDerg <jamesclamey@gmail.com>
2025-07-28 11:40:42 +0100

    Slime block scene

---
+++ scripts/slimeblock.gd
@@ -0,0 +1,32 @@
+@tool
+class_name SlimeBlock
+extends Node2D
+
+
+#region Variables
+@export_range(2, 16) var width:int = 2:
+	set(value):
+		width = value
+		box_shape.size.x = BOX_SCALE_MULT * value
+		sprite.size.x = SPR_SCALE_MULT * value
+		sprite.position.x = SPR_SCALE_MULT * value * -2
+@export_range(2, 16) var height:int = 2:
+	set(value):
+		height = value
+		box_shape.size.y = BOX_SCALE_MULT * value
+		sprite.size.y = SPR_SCALE_MULT * value
+		sprite.position.y = SPR_SCALE_MULT * value * -2
+
+const BOX_SCALE_MULT:int = 96
+const SPR_SCALE_MULT:int = 24
+
+@onready var box:CollisionShape2D = $"CollisionShape2D"
+@onready var sprite:NinePatchRect = $"NinePatchRect"
+@onready var box_shape:RectangleShape2D = box.shape
+#endregion
+
+
+func _ready() -> void:
+	box.shape = RectangleShape2D.new()
+	box_shape = box.shape
+	box_shape.size = Vector2(width, height) * BOX_SCALE_MULT
