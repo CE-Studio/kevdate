4520598a2445cc7c6b9ee5041816d94ee19e2d7e
EpsilonTheDerg <jamesclamey@gmail.com>
2025-08-02 15:50:06 +0100

    Level 1c

---
+++ scripts/PlatformTypes/Ice.gd
@@ -0,0 +1,12 @@
+class_name Ice
+extends Platform
+
+
+func _on_player_enter(body:Node2D) -> void:
+	if body is Player:
+		pass
+
+
+func _on_player_exit(body:Node2D) -> void:
+	if body is Player:
+		pass
